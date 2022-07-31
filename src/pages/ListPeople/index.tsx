96d4c3089fc200798e6a5f6a377d6356adc7b67c
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-31 18:21:28 -0300

    feat: create componente de People

---
+++ src/pages/ListPeople/index.tsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+
+import api from "../../services/api";
+
+import { FilterButton } from "../../components/FilterButton";
+import { Loading } from "../../components/Loading";
+import { IPeople } from "./interface";
+
+import { Container, CardList, Wrapper } from "./styles";
+
+export const ListPeople = () => {
+  const [people, setPeople] = useState<IPeople[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [apiResults, setApiResults] = useState<IPeople[]>([]);
+
+  const handleGenerateRandom = () => {
+    const random = Math.floor(Math.random() * apiResults.length);
+    const { name, height, mass, hair_color, skin_color } = apiResults[random];
+    const newPeople = { name, height, mass, hair_color, skin_color };
+    setPeople([newPeople]);
+  };
+
+  useEffect(() => {
+    async function AllPeoples() {
+      try {
+        const { data } = await api.get(`people`);
+        setApiResults(data.results);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    AllPeoples();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <Container>
+      <Wrapper>
+        <FilterButton onClick={handleGenerateRandom}>
+          Generate random
+        </FilterButton>
+      </Wrapper>
+      <CardList>
+        {people.map((person: IPeople) => (
+          <li key={person?.name}>
+            <strong>Name: {person.name}</strong>
+            <span>Height: {person.height}</span>
+            <span>Mass: {person.mass}</span>
+            <span>Hair_color: {person.hair_color}</span>
+            <span>Skin_color: {person.skin_color}</span>
+          </li>
+        ))}
+      </CardList>
+    </Container>
+  );
+};
---
+++ src/pages/ListPeople/interface.ts
@@ -0,0 +1,7 @@
+export interface IPeople {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+}
