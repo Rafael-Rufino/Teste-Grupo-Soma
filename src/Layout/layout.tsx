c70063bbeccdf8ae87ff60ba318a254bed00288b
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-31 18:33:35 -0300

    feat: create component of layout

---
+++ src/Layout/layout.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import GlobalStyles from "../styles/global";
+
+export function Layout() {
+  return (
+    <>
+      <Header />
+      <GlobalStyles />
+    </>
+  );
+}
